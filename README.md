a7a0b5706ae8971995c6bb8ff36814cc6d8f482c
Darko Trpevski <trpevski@gmail.com>
2023-08-23 11:36:29 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheMovieDbSDK-main
+
+A description of this package.
---
+++ Tests/TheMovieDbSDK-mainTests/TheMovieDbSDK_mainTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TheMovieDbSDK_main
+
+final class TheMovieDbSDK_mainTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TheMovieDbSDK_main().text, "Hello, World!")
+    }
+}
